e6124e2411201cf81eeb57b426b7130809bd1d51
temp98765 <temp98765@gmail.com>
2018-01-08 21:05:21 +0100

    CarSharing extends JFrame

--- src/main/java/gui/CarSharing.java
+++ src/main/java/gui/CarSharing.java
@@ -9,19 +9,10 @@ import gui.action.QuitAction;
 import gui.action.SaveSimulationAction;
 import logic.SimulationState;
 import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.net.URL;
-import javax.imageio.ImageIO;
-import javax.swing.AbstractAction;
 import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
-import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -34,13 +25,11 @@ import javax.swing.SpinnerNumberModel;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-public class CarSharing {
+public class CarSharing extends JFrame {
     private final JMenuItem newSimulation;
     private final JMenuItem openSimulation;
     private final JMenuItem saveSimulation;
     private final JMenuItem quit;
-    
-    private final JFrame frame = new JFrame();
     
     private final JSpinner size = new JSpinner(new SpinnerNumberModel(10, 3, 20, 1));
     private final JButton step = new JButton("Step");
@@ -121,12 +110,12 @@ public class CarSharing {
         container.add(leftColumn, BorderLayout.WEST);
         container.add(canvas, BorderLayout.CENTER);
         
-        frame.setJMenuBar(menuBar);
-        frame.setContentPane(container);
-        frame.setTitle("Car Sharing front");
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.pack();
-        frame.setVisible(true);
+        setJMenuBar(menuBar);
+        setContentPane(container);
+        setTitle("Car Sharing front");
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	pack();
+        setVisible(true);
     }
     
     public static void main(String[] arg) throws Exception {
